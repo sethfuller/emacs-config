40f91a965e0613d6bb0e854c5c72b770b5f9fdac
Seth Fuller <fuller.sethm@gmail.com>
2016-08-03 14:59:08 -0400

    Example of expand file name (to full path)

---
+++ set-full-path.el
@@ -0,0 +1,7 @@
+
+(setq org-base-path (expand-file-name "~/sfuller/bin"))
+
+(setq org-default-notes-file-path (format "%s/%s" org-base-path "notes.org")
+      todo-file-path              (format "%s/%s" org-base-path "gtd.org")
+      journal-file-path           (format "%s/%s" org-base-path "journal.org")
+      today-file-path             (format "%s/%s" org-base-path "2010.org"))
